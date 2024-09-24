c753b428d11e86a6a057192ee784c82df2638841
yasasvivadapalli801382029 <yasasvi09@gmail.com>
2024-09-24 02:54:59 -0400

    Brought Pbudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+        <div className="center">
+            All rights reserved &copy; Yasasvi Vadapalli
+        </div>
+    </footer>
+
+  );
+}
+
+export default Footer;
